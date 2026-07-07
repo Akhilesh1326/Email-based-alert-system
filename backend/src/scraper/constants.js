a931cde43163078d5884f786e4dc5a94beee5df5
Ashwanti <gaikwadashwanti@gmail.com>
2026-07-07 23:48:35 +0530

    small change

--- backend/src/scraper/constants.js
+++ backend/src/scraper/constants.js
@@ -3,6 +3,5 @@ export const COMPANY_LINKS =  {
     "Company B": "https://www.companyb.com/careers",
     "Company C": "https://www.companyc.com/careers",
     "Company D": "https://www.companyd.com/careers",
-    "Company E": "https://www.companye.com/careers"
 };
 
